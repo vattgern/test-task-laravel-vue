c60d14fe45a84475dce7f941788302f4a307b4be
vattgern <sashawot060@gmail.com>
2026-05-01 14:14:15 +0300

    model changes

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -5,10 +5,13 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\Product;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 #[Fillable(['name', 'description'])]
 class Category extends Model
 {
+    use HasFactory;
+
     public function products()
     {
         return $this->hasMany(Product::class);
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -5,10 +5,13 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\Category;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 #[Fillable(['name', 'description', 'price', 'category_id'])]
 class Product extends Model
 {
+    use HasFactory;
+
     public function category()
     {
         return $this->belongsTo(Category::class);
