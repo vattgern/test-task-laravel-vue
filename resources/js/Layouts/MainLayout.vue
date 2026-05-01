18ddf961d2fdefebbb4209c884fad1fda56910b2
vattgern <sashawot060@gmail.com>
2026-05-01 12:12:08 +0300

    front setups

---
+++ resources/js/Layouts/MainLayout.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { Head } from "@inertiajs/vue3";
+
+const props = defineProps({
+    title: String,
+});
+</script>
+
+<template>
+
+    <Head>
+        <title>{{ props.title }}</title>
+    </Head>
+    <slot />
+</template>
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,41 @@
-//
+// APP
+import { h, createApp } from "vue";
+import { createInertiaApp } from "@inertiajs/vue3";
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers'
+import MainLayout from "./Layouts/MainLayout.vue";
+// LIBRARIES
+import PrimeVue from "primevue/config";
+import Aura from "@primeuix/themes/aura";
+
+
+createInertiaApp({
+    resolve: name => {
+        const pages = import.meta.glob('./Pages/**/*.vue', { eager: true });
+        const page = pages[`./Pages/${name}.vue`];
+
+        // Добавьте проверку и логгирование
+        if (!page) {
+            console.error(`Page not found: ./Pages/${name}.vue`);
+            throw new Error(`Page "${name}" not found`);
+        }
+
+        if (page && page.default) {
+            page.default.layout = page.default.layout || MainLayout;
+        }
+
+        return page;
+    },
+    title: title => title ? `${title}` : 'Главная',
+    setup({ el, App, props, plugin }) {
+        const VueApp = createApp({ render: () => h(App, props) })
+            .use(plugin)
+            .use(PrimeVue, {
+                theme: {
+                    preset: Aura
+                }
+            });
+
+        VueApp.mount(el);
+        return VueApp;
+    },
+});
