e5868dea4aaa9e871e772689d3f41a85ca4e7ce1
vattgern <sashawot060@gmail.com>
2026-05-01 12:11:37 +0300

    setting packages && blades

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
+import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
     plugins: [
@@ -8,8 +9,37 @@ export default defineConfig({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
-        tailwindcss(),
+        vue({
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
+                },
+            },
+        }),
     ],
+    build: {
+        sourcemap: true,
+        manifest: true,
+        minify: 'terser',
+        terserOptions: {
+            format: {
+                comments: false
+            },
+            compress: {
+                drop_console: false,
+                drop_debugger: true,
+            }
+        }
+    },
+    css: {
+        devSourcemap: true,
+        preprocessorOptions: {
+            scss: {
+                api: 'modern-compiler'
+            }
+        }
+    },
     server: {
         watch: {
             ignored: ['**/storage/framework/views/**'],
