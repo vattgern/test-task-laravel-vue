d1b9efdea76fe0af2351e2e5a02875e64d87c778
vattgern <sashawot060@gmail.com>
2026-05-03 11:59:16 +0300

    change loader

--- resources/js/Components/Loader.vue
+++ resources/js/Components/Loader.vue
@@ -4,7 +4,7 @@
 
 <template>
     <div
-        style="width: 100vw; height: 100vh;"
+        style="width: 100%; height: 100%;"
         class="absolute top-0 left-0 bg-white flex items-center justify-center"
     >
         <div class="h-8 w-8 animate-spin rounded-full border-4 border-blue-500 border-t-transparent"></div>
